2209552760d3622f8768ea46374ea435077f4368
Maryline Mutai Chebet <mmutai@mtech.ke>
2024-01-12 08:22:11 +0300

    file updates

--- doctor_appointment/lib/screens/appointment_page.dart
+++ doctor_appointment/lib/screens/appointment_page.dart
@@ -13,21 +13,27 @@ FilterStatus status = FilterStatus.upcoming;
 Alignment _alignment = Alignment.centerLeft;
 List<dynamic> schedules = [
   {
-    "doctor_image":"Richard",
+    "doctor_name":"Richard",
     "doctor_profile":"assets/images/doctor1.png",
     "category":"Dental",
     "status":FilterStatus.upcoming,
   },
   {
-    "doctor_image":"Richard",
-    "doctor_profile":"assets/images/doctor1.png",
+    "doctor_name":"Cher",
+    "doctor_profile":"assets/images/doctor.png",
     "category":"cardiology",
     "status":FilterStatus.complete,
   },
   {
-    "doctor_image":"Richard",
-    "doctor_profile":"assets/images/doctor1.png",
-    "category":"DentRespiration",
+    "doctor_name":"Allan",
+    "doctor_profile":"images/doctor.png",
+    "category":"Dental",
+    "status":FilterStatus.complete,
+  },
+  {
+    "doctor_name":"Test",
+    "doctor_profile":"images/doctor.png",
+    "category":"Respiration",
     "status":FilterStatus.cancel,
   }
 
@@ -50,7 +56,7 @@ List<dynamic> schedules = [
       // }
       return schedule['status'] == status ;
     }).toList();
-
+    Config.init(context);
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.only(left: 20, top: 20, right: 20),
@@ -89,26 +95,209 @@ List<dynamic> schedules = [
                                 }
                                 else if (filterStatus == FilterStatus.complete) {
                                   status = FilterStatus.complete;
-                                  _alignment = Alignment.centerLeft;
+                                  _alignment = Alignment.center;
                                 }
                                 else if (filterStatus == FilterStatus.cancel) {
                                   status = FilterStatus.cancel;
-                                  _alignment = Alignment.centerLeft;
+                                  _alignment = Alignment.centerRight;
                                 }
                               });
                             },
-                            child: Center(
-                              child: Text(filterStatus.name),
-                            ),
+                            child:
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 8.0), // Adjust the padding as needed
+                                child: Text(
+                                  filterStatus.name,
+                                ),
+                              ),
+                            )
+
                           ))
                       ],
                     ),
                   ),
+                  AnimatedAlign(
+                      alignment: _alignment,
+                      duration: const Duration(milliseconds: 200),
+                    child: Container(
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Config.primaryColor,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                        child: Text(
+                          status.name,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
                 ],
-              )
+              ),
+              Config.spaceSmall,
+              Expanded(
+                  child: ListView.builder(
+                    itemCount: filteredSchedules.length,
+                      itemBuilder: ((context, index)
+                      {
+                        var _schedule = filteredSchedules[index];
+                        bool islastElement = filteredSchedules.length + 1 == index;
+                        return Card(
+                          shape: RoundedRectangleBorder(
+
+                            side:  BorderSide(
+                              color: Colors.grey,
+                            ),borderRadius: BorderRadius.circular(20)
+                          ),
+                          margin: !islastElement
+                          ? const EdgeInsets.only(bottom: 20)
+                              :EdgeInsets.zero,
+                          child: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Row(
+                                  children: [
+                                    CircleAvatar(
+                                      backgroundImage: AssetImage( _schedule['doctor_profile'] ),
+                                    ),
+                                   SizedBox(
+                                     width: 10,
+                                   ),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          _schedule['doctor_name'] ,
+
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold
+                                          ),
+                                        ),
+                                        SizedBox(height: 5,),
+                                        Text(
+                                          _schedule['category'] ,
+
+                                          style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold
+                                          ),
+                                        ),
+
+                                      ],
+                                    ),
+
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                ScheduleCard(),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Expanded(
+                                      child: ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Colors.white,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(5),
+                                          ),
+                                          fixedSize: const Size(double.infinity, 5),
+                                        ),
+                                        child: const Text(
+                                          'Cancel',
+                                          style: TextStyle(color: Config.primaryColor),
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(width: 5,),
+
+                                    Expanded(
+                                      child: ElevatedButton(
+                                        onPressed: () {},
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor: Config.primaryColor,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(5),
+                                          ),
+                                          fixedSize: const Size(double.infinity, 5),
+                                        ),
+                                        child: const Text(
+                                          'Cancel',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+
+                        );
+                      })
+                  )
+              ),
 
             ],
         ),
+      ),
+    );
+  }
+}
+
+
+class ScheduleCard extends StatelessWidget {
+
+  const ScheduleCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      child: const Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(Icons.calendar_today,
+            color: Config.primaryColor,
+            size: 15,
+          ),
+          SizedBox(width: 5,),
+          Text(
+            'Wednesday 10/1/2023',
+            style: TextStyle(color: Config.primaryColor),
+          ),
+          SizedBox(width: 20,),
+          Icon(Icons.access_alarm,
+            color:Config.primaryColor,
+            size: 17,
+          ),
+          SizedBox(width: 5,),
+          Flexible(
+            child: Text(
+              '12.00 pm',
+              style: TextStyle(color: Config.primaryColor),
+            ),)
+        ],
       ),
     );
   }
